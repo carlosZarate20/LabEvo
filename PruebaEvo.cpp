bc3e94767aee97515f39a4fcb089afa3ff0652d3
Carlos Zarate <u201314894@upc.edu.pe>
2016-04-26 13:24:08 -0500

    first commit

---
+++ PruebaEvo.cpp
@@ -0,0 +1,14 @@
+// PruebaEvo.cpp: archivo de proyecto principal.
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "conio.h"
+
+using namespace std;
+
+int main()
+{
+	printf("HOLA MUNDO: COMO ESTA");
+	_getch();
+    return 0;
+}
